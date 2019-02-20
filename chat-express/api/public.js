26fa52f736c7d53f4983c1dfe6b8bff1a8cddc13
lyaaaa <1007634863@qq.com>
2019-02-20 15:45:03 +0800

    图片上传到七牛云

--- chat-express/api/public.js
+++ chat-express/api/public.js
@@ -3,6 +3,48 @@ const router = express.Router();
 const formidable = require('formidable')
 const fs = require('fs');
 
+// 七牛云配置
+const qiniu = require('qiniu');
+const ACCESS_KEY = ''
+const SECRET_KEY  = ''
+const mac = new qiniu.auth.digest.Mac(ACCESS_KEY, SECRET_KEY)
+
+var qnConfig = new qiniu.conf.Config();
+// 空间对应的机房
+qnConfig.zone = qiniu.zone.Zone_z0;
+
+var formUploader = new qiniu.form_up.FormUploader(qnConfig);
+var putExtra = new qiniu.form_up.PutExtra();
+
+function upToken(){
+   var options = {
+    scope: '',
+    expires: 7200
+  }
+  const putPolicy = new qiniu.rs.PutPolicy(options)
+  const uploadToken = putPolicy.uploadToken(mac)
+  return uploadToken
+}
+
+// 上传图片到地牛云
+function upFileQiniu(qnToken, key, filePath){
+    formUploader.putFile(qnToken, key, filePath, putExtra, function(respErr,
+        respBody, respInfo) {
+        if (respErr) {
+           throw respErr;
+        }
+        if (respInfo.statusCode == 200) {
+            console.log(respBody);
+        } else {
+            console.log(respInfo.statusCode);
+            console.log(respBody);
+        }
+    });
+}
+
+
+
+
 
 router.post('/upimg', function (req, res) {
     var form = new formidable.IncomingForm();
@@ -15,6 +57,8 @@ router.post('/upimg', function (req, res) {
         }
         let imgPath = files.file.path
         let imgName = files.file.name
+        // var qnToken = upToken()
+        // upFileQiniu(qnToken, imgName, imgPath)
         var readerStream = fs.createReadStream(imgPath);
         var writerStream = fs.createWriteStream(`public/${imgName}`);
         readerStream.pipe(writerStream);
